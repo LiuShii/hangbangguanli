fc3ebd399efad872da1a27f29e0b23a06a083318
LiuShii <1164818081@qq.com>
2019-05-05 13:36:58 +0800

    航班管理

---
+++ src/com/company/PassengerInterface.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public interface PassengerInterface {
+
+
+
+    public String getName();
+
+
+
+    public int getBookingNumber();
+
+
+
+    public int getRow();
+
+
+
+    public int getSeatPosition();
+
+}
